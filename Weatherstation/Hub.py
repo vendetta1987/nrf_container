cb57ae9e93338d73887f9a9af303e96104ea41a9
vendetta <vendetta1987@freenet.de>
2023-07-03 20:41:22 +0200

    added Weatherstation

---
+++ Weatherstation/Hub.py
@@ -0,0 +1,141 @@
+import argparse
+import json
+import random
+import sys
+import time
+import traceback
+
+import pigpio
+from nrf24 import *
+
+from MQTTClient import MQTTCLient
+from Weatherstation import WeatherStation
+
+
+def RunWeatherStation(payload: bytes):
+    global ws
+    ws.Deserialize(payload)
+
+    print(f"T={ws.Temperature:.2f}\tH={ws.Humidity:.2f}\t\
+            P={ws.Pressure:.2f}\tSM={ws.SoilMoisture:.2f}\t\
+            ST={ws.SoilTemperature:.2f}\tWD={ws.WindDirection}\t\
+            WS={ws.WindSpeed:.2f}\tR={ws.Rain:.2f}")
+
+    global mqtt_client
+    mqtt_client.Publish("temperature", ws.Temperature)
+    mqtt_client.Publish("humidity", ws.Humidity)
+    mqtt_client.Publish("pressure", ws.Pressure)
+    mqtt_client.Publish("rain", ws.Rain)
+
+    soil = {
+        "moisture": ws.SoilMoisture,
+        "temperature": ws.SoilTemperature
+    }
+    mqtt_client.Publish("soil", json.dumps(soil))
+
+    wind = {
+        "direction": ws.WindDirection,
+        "speed": ws.WindSpeed,
+    }
+    mqtt_client.Publish("wind", json.dumps(wind))
+
+    mqtt_client.Publish("last_update", time.strftime("%c"))
+
+
+def WakePico(nrf: NRF24, timeout_ms: int = 100) -> list[bytes]:
+    nrf.reset_packages_lost()
+    # wake up call
+    # print("sending wake up")
+    nrf.send(b"wake_up")
+
+    try:
+        nrf.wait_until_sent()
+    except TimeoutError:
+        print("Timeout waiting for transmission to complete.")
+
+    # wait for reaction
+    target_time = time.process_time_ns()+(timeout_ms*1000000)
+
+    payload = []
+    woke_up = False
+
+    while not woke_up and (target_time > time.process_time_ns()):
+        if nrf.data_ready():
+            while nrf.data_ready():
+                payload.append(nrf.get_payload())
+                woke_up = True
+
+    return payload
+
+
+if __name__ == "__main__":
+    print("Python NRF24 Simple Receiver Example.")
+    # Parse command line argument.
+    arg_parser = argparse.ArgumentParser(
+        prog="Hub.py", description="WeatherStation hub application with MQTT connection")
+    arg_parser.add_argument('-n', '--hostname', type=str, default='localhost',
+                            help="Hostname for the device running the pigpio daemon.")
+    arg_parser.add_argument('-p', '--port', type=int, default=8888,
+                            help="Port number of the pigpio daemon.")
+    arg_parser.add_argument('--receive_addr', type=str, nargs='?', default='WSone',
+                            help="receiving pipe address (5 ASCII characters)")
+    arg_parser.add_argument('--send_addr', type=str, nargs='?', default='WStwo',
+                            help="sending pipe address (5 ASCII characters)")
+    arg_parser.add_argument('-ce', type=int, nargs='?', default=23,
+                            help="chip enbale pin")
+    arg_parser.add_argument('--channel', type=int, nargs='?', default=100,
+                            help="NRF channel")
+
+    args = arg_parser.parse_args()
+    hostname = args.hostname
+    port = args.port
+    address_rcv = args.receive_addr
+    address_snd = args.send_addr
+    ce_pin = args.ce
+    nrf_channel = args.channel
+    # Verify that address is between 3 and 5 characters.
+    if (not (2 < len(address_rcv) < 6)) or (not (2 < len(address_snd) < 6)):
+        print(
+            f'Addresses must be between 3 and 5 ASCII characters. receive={address_rcv} send={address_snd}')
+        sys.exit(1)
+
+    print(f'Connecting to GPIO daemon on {hostname}:{port} ...')
+    pi_gpio_d = pigpio.pi(hostname, port)
+
+    if not pi_gpio_d.connected:
+        print("Not connected to Raspberry Pi ... goodbye.")
+        sys.exit()
+
+    nrf = NRF24(pi_gpio_d, ce=ce_pin, payload_size=RF24_PAYLOAD.MAX, channel=nrf_channel,
+                data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.MAX, address_bytes=len(address_rcv))
+    nrf.open_reading_pipe(RF24_RX_ADDR.P1, address_rcv)
+    nrf.open_writing_pipe(address_snd)
+    nrf.show_registers()
+
+    ws = WeatherStation()
+
+    mqtt_client = MQTTCLient()
+    mqtt_client.Connect()
+
+    try:
+        i = 0
+        while True:
+            payload = WakePico(nrf, 200)
+
+            if len(payload) > 0:
+                for packet in payload:
+                    print(f"received {packet}")
+                    RunWeatherStation(packet)
+                print(f"took {i} attempts")
+                i = 0
+                time.sleep(3)
+            else:
+                i += 1
+                time.sleep(0.01*random.randrange(1, 10))
+
+    except:
+        traceback.print_exc()
+        print("powering down")
+        mqtt_client.Publish("status", "disconnected")
+        nrf.power_down()
+        pi_gpio_d.stop()
---
+++ Weatherstation/Weatherstation.py
@@ -0,0 +1,258 @@
+import struct
+
+try:
+    from BME280.Manager import BMEManager
+except:
+    bme_available = False
+else:
+    bme_available = True
+
+try:
+    from CAPSHYG.Manager import SoilMoistureManager
+except:
+    soil_available = False
+else:
+    soil_available = True
+
+try:
+    from DS18B20.Manager import DS18B20Manager
+except:
+    ds18_available = False
+else:
+    ds18_available = True
+
+try:
+    from DS15901.WindVane import WindVane
+    from DS15901.TriggerCounter import TriggerCounter
+except:
+    ds15_available = False
+else:
+    ds15_available = True
+
+
+class WeatherStation:
+    _temperature: float
+    _humidity: float
+    _pressure: float
+    _soil_moisture: float
+    _soil_temperature: float
+    _wind_direction: str
+    _wind_speed: float
+    _rain: float
+
+    def __init__(self):
+        if bme_available:
+            self._bme = BMEManager(0, 2, 3, 4, 1)
+        else:
+            self._bme = None
+            self._temperature = -1
+            self._humidity = -1
+            self._pressure = -1
+
+        if soil_available:
+            self._soil_M = SoilMoistureManager(27)
+        else:
+            self._soil_M = None
+            self._soil_moisture = -1
+
+        if ds18_available:
+            self._soil_T = DS18B20Manager(13)
+        else:
+            self._soil_T = None
+            self._soil_temperature = -1
+
+        if ds15_available:
+            self._wind_vane = WindVane(26)
+            self._wind_anemometer = TriggerCounter(2.4, 22)
+            self._rain_gauge = TriggerCounter(0.2794, 16)
+        else:
+            self._wind_vane = None
+            self._wind_anemometer = None
+            self._rain_gauge = None
+            self._wind_direction = "X"
+            self._wind_speed = -1
+            self._rain = -1
+
+    @property
+    def Temperature(self) -> float:
+        if self._bme is not None:
+            return self._bme.Temperature
+        else:
+            return self._temperature
+
+    @Temperature.setter
+    def Temperature(self, val: float):
+        self._temperature = val
+
+    @property
+    def Humidity(self) -> float:
+        if self._bme is not None:
+            return self._bme.Humidity
+        else:
+            return self._humidity
+
+    @Humidity.setter
+    def Humidity(self, val: float):
+        self._humidity = val
+
+    @property
+    def Pressure(self) -> float:
+        if self._bme is not None:
+            return self._bme.Pressure
+        else:
+            return self._pressure
+
+    @Pressure.setter
+    def Pressure(self, val: float):
+        self._pressure = val
+
+    @property
+    def SoilMoisture(self) -> float:
+        if self._soil_M is not None:
+            return self._soil_M.Moisture
+        else:
+            return self._soil_moisture
+
+    @SoilMoisture.setter
+    def SoilMoisture(self, val: float):
+        self._soil_moisture = val
+
+    @property
+    def SoilTemperature(self) -> float:
+        if self._soil_T is not None:
+            return self._soil_T.Temperature
+        else:
+            return self._soil_temperature
+
+    @SoilTemperature.setter
+    def SoilTemperature(self, val: float):
+        self._soil_temperature = val
+
+    @property
+    def WindDirection(self) -> str:
+        if self._wind_vane is not None:
+            return self._wind_vane.Direction
+        else:
+            return self._wind_direction
+
+    @WindDirection.setter
+    def WindDirection(self, val: str):
+        self._wind_direction = val
+
+    @property
+    def WindSpeed(self) -> float:
+        if self._wind_anemometer is not None:
+            return self._wind_anemometer.EvaluatedCount
+        else:
+            return self._wind_speed
+
+    @WindSpeed.setter
+    def WindSpeed(self, val: float):
+        self._wind_speed = val
+
+    @property
+    def Rain(self) -> float:
+        if self._rain_gauge is not None:
+            return self._rain_gauge.EvaluatedCount
+        else:
+            return self._rain
+
+    @Rain.setter
+    def Rain(self, val: float):
+        self._rain = val
+
+    def Serialize(self) -> list[bytes]:
+        packed_structs = []
+        # !  network byte order
+        # B  number of bytes denoting the type
+        # ns n digits as an array/string
+        # B  number of bytes denoting the value
+        # f  a float value
+        packed_structs.append(struct.pack(
+            "!B1sBf", 1, b"T", 4, self.Temperature))
+        packed_structs.append(struct.pack("!B1sBf", 1, b"H", 4, self.Humidity))
+        packed_structs.append(struct.pack("!B1sBf", 1, b"P", 4, self.Pressure))
+        packed_structs.append(struct.pack(
+            "!B2sBf", 2, b"SM", 4, self.SoilMoisture))
+        packed_structs.append(struct.pack(
+            "!B2sBf", 2, b"ST", 4, self.SoilTemperature))
+        
+        wdLen = len(self.WindDirection)
+        packed_structs.append(struct.pack(f"!B2sB{wdLen}s", 2, b"WD", wdLen,
+                                          self.WindDirection.encode("ascii")))
+        
+        packed_structs.append(struct.pack(
+            "!B2sBf", 2, b"WS", 4, self.WindSpeed))
+        packed_structs.append(struct.pack("!B1sBf", 1, b"R", 4, self.Rain))
+
+        packets = []
+        idx = 0
+        struct_cnt = len(packed_structs)
+        while idx < struct_cnt:
+            packet = bytes()
+            while (idx < struct_cnt) and ((len(packed_structs[idx])+len(packet)) < 31):
+                packet = bytes().join([packet, packed_structs[idx]])
+                idx += 1
+            packet = bytes().join([struct.pack("!B", len(packet)), packet])
+            packets.append(packet)
+
+        length = 0
+        for p in packets:
+            length += len(p)
+
+        print(f"serialized {length} bytes in {len(packets)} packets")
+        return packets
+
+    def Deserialize(self, packet: bytes):
+        packet_byte_cnt = struct.unpack("!B", packet[:1])[0]
+        packet = packet[1:]
+        consumed_cnt = 0
+        while consumed_cnt < packet_byte_cnt:
+            type_byte_cnt = struct.unpack("!B", packet[:1])[0]
+            packet = packet[1:]
+            consumed_cnt += 1
+
+            property_type = struct.unpack(
+                f"!{type_byte_cnt}s", packet[:type_byte_cnt])[0]
+            packet = packet[type_byte_cnt:]
+            consumed_cnt += type_byte_cnt
+
+            value_byte_cnt = struct.unpack("!B", packet[:1])[0]
+            packet = packet[1:]
+            consumed_cnt += 1
+
+            if value_byte_cnt == 4:
+                #float value
+                value = struct.unpack("!f", packet[:value_byte_cnt])[0]
+            else:
+                #some string
+                value = struct.unpack(
+                    f"!{value_byte_cnt}s", packet[:value_byte_cnt])[0]
+
+            packet = packet[value_byte_cnt:]
+            consumed_cnt += value_byte_cnt
+            
+            if property_type == b"H":
+                self.Humidity = value
+            elif property_type == b"T":
+                self.Temperature = value
+            elif property_type == b"P":
+                self.Pressure = value
+            elif property_type == b"SM":
+                self.SoilMoisture = value
+            elif property_type == b"ST":
+                self.SoilTemperature = value
+            elif property_type == b"WD":
+                self.WindDirection = value.decode("ascii")
+            elif property_type == b"WS":
+                self.WindSpeed = value
+            elif property_type == b"R":
+                self.Rain = value
+            else:
+                pass
+
+
+if __name__ == "__main__":
+    ws = WeatherStation()
+    packets = ws.Serialize()
+    ws.Deserialize(packets[0])
