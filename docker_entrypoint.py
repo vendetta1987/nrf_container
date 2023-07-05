ce97612338fcdb718e201600d30e166ab7a797fc
vendetta <vendetta1987@freenet.de>
2023-07-05 20:21:00 +0200

    added chdir to CreateProcess; changed output handling and order of script execution

--- docker_entrypoint.py
+++ docker_entrypoint.py
@@ -3,15 +3,19 @@ import subprocess
 
 
 def CreateProcess(args: list[str], cwd: str = ".") -> subprocess.Popen:
+    old_dir = os.getcwd()
     os.chdir(cwd)
     print("running: "+" ".join(args)+" inside "+os.getcwd())
-    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, text=True)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, cwd=cwd, text=True)
+    os.chdir(old_dir)
+    return proc
 
 
 def RunAsProcess(args: list[str], cwd: str = ".", timeout: int = 5) -> bool:
-    CreateProcess(args, cwd)
+    proc = CreateProcess(args, cwd)
 
-    res = True
+    result = True
     stdout = None
     stderr = None
 
@@ -19,29 +23,46 @@ def RunAsProcess(args: list[str], cwd: str = ".", timeout: int = 5) -> bool:
         stdout, stderr = proc.communicate(None, timeout)
     except subprocess.TimeoutExpired:
         print("timeout")
+
         proc.kill()
-        stdout, stderr = proc.communicate()
-    except:
-        res = False
+        _stdout, _stderr = proc.communicate()
 
-    if stderr is not None:
-        res &= len(str(stderr)) <= 0
+        if stdout is None:
+            stdout = _stdout
+        else:
+            stdout += "\n"+_stdout
 
-    if not res and str(stderr):
-        print(f"stderr = {stderr}")
-    elif str(stdout):
-        print(f"stdout = {stdout}")
+        if stderr is None:
+            stderr = _stderr
+        else:
+            stderr += "\n"+_stderr
 
-    return res
+        result = False
+    except Exception as exc:
+        print("exception occured:\n"+exc)
+        result = False
+
+    if stdout is not None and len(str(stdout)) > 0:
+        print(f"stdout:\n{stdout}")
+
+    if stderr is not None and len(str(stderr)) > 0:
+        print(f"stderr:\n{stderr}")
+        result = False
+
+    return result
 
 
 if __name__ == "__main__":
     while True:
-        RunAsProcess(["python", "-m", "hoymiles", "-c",
-                     "/ahoy_work/ahoy.yml"], "/ahoy", 10*60)
+        RunAsProcess(["bash", "spi_reset.sh"], "/weatherstation")
 
-        pigpiod_process = CreateProcess(["pigpiod"])
-        RunAsProcess(["python", "Hub.py"], "/weatherstation", 60)
-        pigpiod_process.kill()
+        RunAsProcess(["python", "-m", "hoymiles", "-c",
+                      "/ahoy_work/ahoy.yml"], "/ahoy", 10)  # *60)
 
         RunAsProcess(["bash", "spi_reset.sh"], "/weatherstation")
+
+        pigpiod_process = CreateProcess(["pigpiod"])
+        RunAsProcess(["python", "Hub.py"], "/weatherstation", 10)
+        pigpiod_process.kill()
+
+        break
