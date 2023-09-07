39ebd639433cc6df0ebc65e5c2a0efb918302820
vendetta <vendetta1987@freenet.de>
2023-09-07 20:12:25 +0200

    updated to Weatherstation and hub 81d3760491b5da3d1d49426bcbf07612a60de5eb

--- Weatherstation/Hub.py
+++ Weatherstation/Hub.py
@@ -90,8 +90,11 @@ def WakePico(nrf: RF24, timeout_ms: int = 100) -> list[bytes]:
     result = nrf.write(b"wake_up")
 
     if not result:
-        print("Timeout during wakeup call")
+        print(".", end="")
+        sys.stdout.flush()
         return []
+    else:
+        print("")
     # wait for reaction
     target_time = time.process_time_ns()+(timeout_ms*1000000)
 
@@ -150,15 +153,15 @@ if __name__ == "__main__":
 
             if len(payload) > 0:
                 for packet in payload:
-                    #print(f"received {packet}")
                     ForwardWeatherStationData(ws, mqtt_client, packet)
-                #print(f"took {i} attempts")
+                print(f"took {i} attempts")
                 i = 0
                 #time.sleep(3)
-                Shutdown()
+                break
             else:
                 i += 1
                 time.sleep(0.01*random.randrange(1, 10))
     except:
-        #traceback.print_exc()
-        Shutdown()
+        traceback.print_exc()
+
+    Shutdown()
